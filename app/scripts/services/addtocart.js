f30c440e2074afbb8315325c5b783d980e25883e
quyen91 <chibedethuong91@gmail.com>
2016-06-23 21:30:28 +0700

    save cart to localstorage

--- app/scripts/services/addtocart.js
+++ app/scripts/services/addtocart.js
@@ -6,33 +6,56 @@
  * @description
  * # addtoCart
  * Service in the angularShopingCartApp.
+ *
+ * Save ID book to localstorage array
+
+
+
+
  */
 angular.module('angularShopingCartApp')
   .service('addtoCart', function (store) {
     // AngularJS will instantiate a singleton by calling "new" on this function
     this.addtocart = function(bookid){
-    	alert('sdsd');
-    	var book1 = {};
-    	// add this to cart
-    	var ref = firebase.database().ref();
-        // get cart id of user. One user have one cart
-        // Check if is first visit page or exist visitor
-        if(store.get('userCartKey')){
-        	alert('exist')
-        	var newCartKey = store.get('userCartKey');
-        }else{
-        	var newCartKey = firebase.database().ref().child('carts').push().key;
-       		store.set('userCartKey', newCartKey);
-        }
+      
+      var cartList = [];
+      
+      if(store.get('cartList')){ 
+        cartList = store.get('cartList');
+        if(cartList.indexOf(bookid) == -1){
+          cartList.push(bookid);
+          store.set('cartList', cartList);
+          alert('Add successfully');
+        }else{alert('Already added to cart');}
+
+      }else{
+        cartList.push(bookid);
+        store.set('cartList', cartList);
+        alert('Add successfully');
+      } 
+
+       
+      // var book1 = {};
+      // // add this to cart
+      // var ref = firebase.database().ref();
+     //    // get cart id of user. One user have one cart
+     //    // Check if is first visit page or exist visitor
+     //    if(store.get('userCartKey')){
+     //     alert('exist')
+     //     var newCartKey = store.get('userCartKey');
+     //    }else{
+     //     var newCartKey = firebase.database().ref().child('carts').push().key;
+     //       store.set('userCartKey', newCartKey);
+     //    }
         
-        console.log(newCartKey);
+     //    console.log(newCartKey);
 
-        var updates ={};
-        book1.title = 'erer';
-        book1.price = '123';
+     //    var updates ={};
+     //    book1.title = 'erer';
+     //    book1.price = '123';
 
-        updates['/carts/' + newCartKey + '/' + bookid] = book1;
-        ref.update(updates);
+     //    updates['/carts/' + newCartKey + '/' + bookid] = book1;
+     //    ref.update(updates);
     }
 
 

ff4e4b039af14e33d6e4741dae4b86cf5816d69d
khanh <tmkhanh69@gmail.com>
2016-06-23 23:29:51 +0700

    test2

--- app/scripts/controllers/cart.js
+++ app/scripts/controllers/cart.js
@@ -6,12 +6,47 @@
  * @description
  * # CartCtrl
  * Controller of the angularShopingCartApp
+ * 
+ *
+ * Display Cart realtime from firebase with id in localStorage
+ *
+ *
+ *
+ *
+
+
+
  */
 angular.module('angularShopingCartApp')
-  .controller('CartCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('CartCtrl', function ($scope, auth, store, $http, $state, $stateParams) {
+
+   var cartList = [];
+   cartList = store.get('cartList');
+   $scope.book = {};
+   $scope.arrayCart = [];
+   var ref1 = firebase.database().ref('books');
+
+         // alert(angular.toJson(snapshot.val()));
+      for(var i=0; i<cartList.length; i++){
+   
+         firebase.database().ref('books/' + cartList[i]).on('value', function(snapshot){
+            $scope.book = snapshot.val();
+            $scope.book.id = cartList[i];
+            $scope.arrayCart.push($scope.book);
+            $scope.$apply();
+         });
+         
+      }
+
+console.log($scope.arrayCart);
+
+       // $scope.book = snapshot.val();
+       // $scope.arrayCart.push($scope.book);
+       // $scope.$apply();
+       // alert(angular.toJson($scope.book));     
+
+
+   
+
+    
   });

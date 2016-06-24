e11e5a6448b053e487c3da40c84667951b2bf80a
khanh <tmkhanh69@gmail.com>
2016-06-24 14:40:49 +0700

    make realtime cart

--- app/scripts/controllers/cart.js
+++ app/scripts/controllers/cart.js
@@ -9,7 +9,7 @@
  * 
  *
  * Display Cart realtime from firebase with id in localStorage
- *
+ * Upload all 
  *
  *
  *
@@ -18,27 +18,73 @@
 
  */
 angular.module('angularShopingCartApp')
-  .controller('CartCtrl', function ($scope, auth, store, $http, $state, $stateParams) {
-
-   var cartList = [];
-   cartList = store.get('cartList');
+  .controller('CartCtrl', function ($scope, auth, store, $http, $state, $stateParams,  $filter) {
+    // identify this is your cart
+   var newCartKey = store.get('userCartKey');
+   $scope.listID = {};
    $scope.book = {};
-   $scope.arrayCart = [];
-   var ref1 = firebase.database().ref('books');
+   var refCart = firebase.database().ref().child('carts/' + newCartKey);
 
-         // alert(angular.toJson(snapshot.val()));
-      for(var i=0; i<cartList.length; i++){
-   
-         firebase.database().ref('books/' + cartList[i]).on('value', function(snapshot){
-            $scope.book = snapshot.val();
-            $scope.book.id = cartList[i];
-            $scope.arrayCart.push($scope.book);
-            $scope.$apply();
-         });
+   refCart.on('value', function(snapshot){
+      $scope.listID = snapshot.val(); 
+      $scope.arrayCart = [];
+     
+      angular.forEach($scope.listID, function(value, key){  // key is bookid
+        firebase.database().ref().child('books/' + key).on('value',function(snapshot){
+          $scope.book = snapshot.val();
+          $scope.book.bookid = key;
+       
+        
+          // $scope.arrayCart.push($scope.book);
+
+          //filter the array
+          var foundItem = $filter('filter')($scope.arrayCart, { bookid: key }, true)[0];
+          //get the index
+          var index = $scope.arrayCart.indexOf(foundItem);
+        
+          if(index == -1){
+             $scope.arrayCart.push($scope.book);
+          }else{
+            $scope.arrayCart[index] = $scope.book;
+            // $scope.arrayCart.splice(index, 0, $scope.book);
+
+          }
+          
+          $scope.$apply();
+
+        });
+        
+      });
+      $scope.$apply();
+
+   });
+
+
+
+   // var ref1 = firebase.database().ref('books');
+  
+   // ref1.on('value', function(snapshot){
+
+   //    cartList = store.get('cartList');
+   //    // alert(angular.toJson(snapshot.val()));
+   //    for(var i=0; i<cartList.length; i++){
+   //       firebase.database().ref('books/' + cartList[i]).on('value', function(snapshot){
+           
+   //          $scope.book = snapshot.val();          
+   //          $scope.arrayCart.push($scope.book);
+   //          // $scope.arrayCart[i] = $scope.book;
+   //          $scope.$apply();
+
+        
+           
+   //       });
          
-      }
+   //    }
 
-console.log($scope.arrayCart);
+   // });
+      
+
+
 
        // $scope.book = snapshot.val();
        // $scope.arrayCart.push($scope.book);
